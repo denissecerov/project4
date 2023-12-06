c624ba82df5a941b4f5533e5a7e2db02c70fe657
denissecerov <130935354+denissecerov@users.noreply.github.com>
2023-12-06 18:02:57 -0500

    buyer DB
    Basic Buyer dashboard start with some images incl javascript.

---
+++ buyerdashboard.js
@@ -0,0 +1,11 @@
+function searchProperties() {
+    console.log("Searching properties...");
+}
+
+function viewPropertyDetails(propertyId) {
+    console.log(`Viewing details for property ${propertyId}`);
+}
+
+function addToWishlist(propertyId) {
+    console.log(`Added property ${propertyId} to wishlist`);
+}
