875c8089b211d7a1d39e6222badcf5e5a5230f47
denissecerov <130935354+denissecerov@users.noreply.github.com>
2023-12-08 15:02:03 -0500

    Search function
    Need to adjust how the cards function in html but you can search from the array created in const properties.

--- buyerdashboard.js
+++ buyerdashboard.js
@@ -1,11 +1,65 @@
+const properties = [
+    { id: 1, title: 'Luxury Apartment', location: 'Downtown', price: '$1,500,000' },
+    { id: 2, title: 'Townhome', location: 'Suburb', price: '$500,000' },
+    { id: 3, title: 'Modern Condo', location: 'Downtown', price: '$900,000' },
+    // Add more properties as needed
+];
+
 function searchProperties() {
-    console.log("Searching properties...");
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+
+    // Filter properties based on search input
+    const searchResults = properties.filter(property =>
+        property.title.toLowerCase().includes(searchInput) ||
+        property.location.toLowerCase().includes(searchInput)
+    );
+
+    // Clear existing property cards
+    clearPropertyCards();
+
+    // Display search results
+    displayPropertyCards(searchResults);
 }
 
+function displayPropertyCards(propertyList) {
+    const propertyCardsContainer = document.querySelector('.property-cards');
+
+    propertyList.forEach(property => {
+        const propertyCard = createPropertyCard(property);
+        propertyCardsContainer.appendChild(propertyCard);
+    });
+}
+
+function createPropertyCard(property) {
+    const propertyCard = document.createElement('div');
+    propertyCard.classList.add('property-card');
+    propertyCard.onclick = function() {
+        viewPropertyDetails(property.id);
+    };
+
+    // Populate card content
+    propertyCard.innerHTML = `
+        <img src="property_image${property.id}.jpeg" alt="${property.title}">
+        <h3>${property.title}</h3>
+        <p>Location: ${property.location}</p>
+        <p>Price: ${property.price}</p>
+        <button onclick="addToWishlist(${property.id})">Add to Wishlist</button>
+    `;
+
+    return propertyCard;
+}
+
+function clearPropertyCards() {
+    const propertyCardsContainer = document.querySelector('.property-cards');
+    propertyCardsContainer.innerHTML = ''; // Clear existing cards
+}
+
+// Sample function to view property details
 function viewPropertyDetails(propertyId) {
     console.log(`Viewing details for property ${propertyId}`);
 }
 
+// Sample function to add to wishlist
 function addToWishlist(propertyId) {
     console.log(`Added property ${propertyId} to wishlist`);
 }
