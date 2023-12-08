15244e024c07f4e88b779fbd4ecdab1c826dcb58
bebe73737 <147670283+bebe73737@users.noreply.github.com>
2023-12-08 01:26:23 -0500

    Update login.php

--- login.php
+++ login.php
@@ -8,39 +8,30 @@ $dbname = "rpatel245";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 
-if ($conn->connect_error) 
-{
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") 
-{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT * FROM users WHERE username='$username'";
-    $result = $conn->query($sql);
+    $stmt = $conn->prepare("SELECT password FROM users WHERE username=?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-    if ($result->num_rows == 1) 
-    {
+    if ($result->num_rows == 1) {
         $row = $result->fetch_assoc();
-        if (password_verify($password, $row['password'])) 
-        {
+        if (password_verify($password, $row['password'])) {
             $_SESSION['username'] = $username;
             header("Location: dashboard.php");
             exit();
-        } 
-        else 
-        {
-            echo "Invalid username or password!";
         }
-    } 
-    else 
-    {
-        echo "Invalid username or password!";
     }
+    echo "Invalid username or password!";
 }
 
 $conn->close();
-
 ?>
+
